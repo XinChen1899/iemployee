d97dc7b292b8499432feecf413e54d12551845de
陈昕 <1932296621@qq.com>
2021-03-14 10:18:58 +0800

    Add files via upload

---
+++ iemployee/src/org/zuel/iemployee/demo/constant/PostConstant.java
@@ -0,0 +1,17 @@
+package org.zuel.iemployee.demo.constant;
+
+/**
+ * create time: 2021/3/10 21:41
+ * author: XinChen1899
+ */
+
+public class PostConstant {
+
+    // 帖子发布失败
+    public static final int POST_UNSUCCESSFULLY_CODE = 2001;
+    public static final String POST_UNSUCCESSFULLY_ERROR = "贴子发布失败";
+
+    // 帖子不存在
+    public static final int POST_NOT_EXIST_CODE = 2002;
+    public static final String POST_NOT_EXIST_ERROR = "帖子不存在";
+}
