830b916bee3e564bbb69cb84bf6ae68275478ea6
陈昕 <1932296621@qq.com>
2021-03-17 09:27:11 +0800

    Add files via upload

--- iemployee/src/org/zuel/iemployee/demo/constant/PostConstant.java
+++ iemployee/src/org/zuel/iemployee/demo/constant/PostConstant.java
@@ -14,4 +14,16 @@ public class PostConstant {
     // 帖子不存在
     public static final int POST_NOT_EXIST_CODE = 2002;
     public static final String POST_NOT_EXIST_ERROR = "帖子不存在";
+
+    // 帖子已存在
+    public static final int POST_EXIST_CODE = 2003;
+    public static final String POST_EXIST_ERROR = "帖子已存在";
+
+    // 帖子删除失败
+    public static final int POST_DELETE_UNSUCCESSFULLY_CODE = 2004;
+    public static final String POST_DELETE_UNSUCCESSFULLY_ERROR = "帖子删除失败";
+
+    // 帖子修改失败
+    public static final int POST_UPDATE_UNSUCCESSFULLY_CODE = 2005;
+    public static final String POST_UPDATE_UNSUCCESSFULLY_ERROR = "帖子修改失败";
 }
--- iemployee/src/org/zuel/iemployee/demo/constant/UserConstant.java
+++ iemployee/src/org/zuel/iemployee/demo/constant/UserConstant.java
@@ -1,5 +1,6 @@
 package org.zuel.iemployee.demo.constant;
 
+
 /**
  * create time: 2021/3/10 21:39
  * author: XinChen1899
@@ -8,22 +9,22 @@ package org.zuel.iemployee.demo.constant;
 public class UserConstant {
 
     // 用户名已存在
-    public static final int USERNAME_EXIST_CODE = 1001;
+    public static final int USERNAME_EXIST_CODE = 0001;
     public static final String USERNAME_EXIST_ERROR = "用户名已存在";
 
     // 用户名不存在
-    public static final int USERNAME_NOT_EXIST_CODE = 1002;
+    public static final int USERNAME_NOT_EXIST_CODE = 0002;
     public static final String USERNAME_NOT_EXIST_ERROR = "用户名不存在";
 
     // 账号或密码错误
-    public static final int USER_INVALID_CODE = 1003;
+    public static final int USER_INVALID_CODE = 0003;
     public static final String USER_INVALID_ERROR = "账号或密码错误";
 
     // 用户未认证
-    public static final int USER_NOT_CERTIFICATED_CODE = 1004;
+    public static final int USER_NOT_CERTIFICATED_CODE = 0004;
     public static final String USER_NOT_CERTIFICATED_ERROR = "用户未认证";
 
     // 用户认证失败
-    public static final int USER_CERTIFICATED_UNSUCCESSFULLY_CODE = 1005;
+    public static final int USER_CERTIFICATED_UNSUCCESSFULLY_CODE = 0005;
     public static final String USER_CERTIFICATED_UNSUCCESSFULLY_ERROR = "用户认证失败";
 }
